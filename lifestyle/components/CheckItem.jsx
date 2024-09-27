080f05b2bf07940e74844388887b019a928edfd7
campbeo2 <campbeo2@miamioh.edu>
2024-09-26 21:45:16 -0400

    add swipe css (no function)

--- lifestyle/components/CheckItem.jsx
+++ lifestyle/components/CheckItem.jsx
@@ -1,17 +1,33 @@
-import { View, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet, TextInput, Text, Alert } from "react-native";
 import { useState, useEffect } from "react";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { Colors } from "../constants/Colors";
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+
 
 const CheckItem = ({ text, setCurrList, currList, index }) => {
     const [isChecked, setIsChecked] = useState(false);
     const [displayText, setDisplayText] = useState(text);
 
+    const deleteItem = () => {
+        var update = [...currList];
+        update.splice(index, 1);
+        setCurrList(update);
+    }
+
     const editItem = (input) => {
+        console.log("in", input)
         if (input === "") {
+            console.log("display", displayText)
+            if (displayText.length <= 1) {
+                deleteItem();
+            }
+
             console.log("empty")
             console.log(isChecked)
             setIsChecked(false);
+
+            return
         }
         var update = currList;
         update[index].name = input;
@@ -38,23 +54,59 @@ const CheckItem = ({ text, setCurrList, currList, index }) => {
         setCurrList(update);
     }
 
+
+      const rightSwipeActions = () => {
+        return (
+          <View
+            style={{
+              backgroundColor: '#dd2150',
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+            }}
+          >
+            <Text
+              style={{
+                color: '#fff',
+                paddingHorizontal: 10,
+                fontWeight: '600',
+                padding: 30,
+              }}
+            >
+              Delete
+            </Text>
+          </View>
+        )
+      }
+    
+      const swipeFromRightOpen = () => {
+        Alert.alert('Swipe from right')
+      }
+
     useEffect(() => {
         setDisplayText(text);
     }, [text]);
     
     return (
-        <View style={styles.listItemView}>
-            <View style={styles.listBoxView}>
-                <BouncyCheckbox fillColor="#9342f5" onPress={checkItem} isChecked={isChecked}/>
+        <Swipeable
+        renderRightActions={rightSwipeActions}
+        onSwipeableOpen={(direction) => {
+          console.log(direction) // "left" | "right"
+          direction === 'left' ? swipeFromLeftOpen() : swipeFromRightOpen()
+        }}
+      >
+            <View style={styles.listItemView}>
+                <View style={styles.listBoxView}>
+                    <BouncyCheckbox fillColor="#9342f5" onPress={checkItem} isChecked={isChecked}/>
+                </View>
+                <TextInput
+                    style={[styles.text, {textDecorationLine: isChecked ? "line-through": "none"}]}
+                    value={displayText}
+                    onChangeText={(input) => {editItem(input)}}
+                    onSubmitEditing={newItem}
+                    selectionColor={Colors.dark.primary}
+                />
             </View>
-            <TextInput
-                style={[styles.text, {textDecorationLine: isChecked ? "line-through": "none"}]}
-                value={displayText}
-                onChangeText={(input) => {editItem(input)}}
-                onSubmitEditing={newItem}
-                selectionColor={Colors.dark.primary}
-            />
-        </View>
+        </Swipeable>
       );
 }
 
