36d463cad38f510748c59e8b9cbc5d5e7b8ee171
campbeo2 <campbeo2@miamioh.edu>
2024-10-10 09:59:27 -0400

    add haptics to swipeable (unfinished)

--- lifestyle/components/CheckItem.jsx
+++ lifestyle/components/CheckItem.jsx
@@ -1,14 +1,38 @@
-import { View, StyleSheet, TextInput, Text, Alert } from "react-native";
+import { Dimensions, View, StyleSheet, TextInput } from "react-native";
 import { useState, useEffect, useRef } from "react";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { Colors } from "../constants/Colors";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import { useSharedValue, runOnJS } from "react-native-reanimated";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Haptics from "expo-haptics";
 
 const CheckItem = ({ text, setCurrList, currList, index, i }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [displayText, setDisplayText] = useState(text);
+  const hasReachedThreshold = useSharedValue(false);
+  const windowWidth = Dimensions.get("window").width;
+  const threshold = windowWidth / 3;
+  const offsetX = useSharedValue(0);
+  const startX = useSharedValue(0);
+
   const swipeableRef = useRef(null);
+  const panRef = useRef();
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      if (offsetX.value < threshold) {
+        if (hasReachedThreshold.value === false) {
+          runOnJS(Haptics.impactAsync)(Haptics.ImpactFeedbackStyle.Medium);
+          hasReachedThreshold.value = true;
+        }
+      } else {
+        hasReachedThreshold.value = false;
+      }
+      offsetX.value = e.translationX + startX.value;
+    })
+    .withRef(panRef);
 
   const deleteItem = () => {
     var update = [...currList];
@@ -87,40 +111,43 @@ const CheckItem = ({ text, setCurrList, currList, index, i }) => {
   }, [text]);
 
   return (
-    <Swipeable
-      renderRightActions={rightSwipeActions}
-      onSwipeableOpen={(direction) => {
-        direction === "left" ? swipeFromLeftOpen() : swipeFromRightOpen();
-      }}
-      rightThreshold={25}
-      ref={swipeableRef}
-    >
-      <View style={styles.listItemView}>
-        <View>
-          <BouncyCheckbox
-            fillColor="#9342f5"
-            onPress={checkItem}
-            isChecked={isChecked}
+    <GestureDetector gesture={panGesture}>
+      <Swipeable
+        renderRightActions={rightSwipeActions}
+        onSwipeableOpen={(direction) => {
+          direction === "left" ? swipeFromLeftOpen() : swipeFromRightOpen();
+        }}
+        simultaneousHandlers={panRef}
+        rightThreshold={25}
+        ref={swipeableRef}
+      >
+        <View style={styles.listItemView}>
+          <View>
+            <BouncyCheckbox
+              fillColor="#9342f5"
+              onPress={checkItem}
+              isChecked={isChecked}
+            />
+          </View>
+          <TextInput
+            style={[
+              styles.text,
+              { textDecorationLine: isChecked ? "line-through" : "none" },
+              { outline: "none" },
+            ]}
+            value={displayText}
+            onChangeText={(input) => {
+              editItem(input);
+            }}
+            onSubmitEditing={newItem}
+            selectionColor={Colors.dark.primary}
+            onKeyPress={(event) => {
+              deleteRow(event.nativeEvent.key);
+            }}
           />
         </View>
-        <TextInput
-          style={[
-            styles.text,
-            { textDecorationLine: isChecked ? "line-through" : "none" },
-            { outline: "none" },
-          ]}
-          value={displayText}
-          onChangeText={(input) => {
-            editItem(input);
-          }}
-          onSubmitEditing={newItem}
-          selectionColor={Colors.dark.primary}
-          onKeyPress={(event) => {
-            deleteRow(event.nativeEvent.key);
-          }}
-        />
-      </View>
-    </Swipeable>
+      </Swipeable>
+    </GestureDetector>
   );
 };
 
